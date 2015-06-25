0771402974804bd49211c070d2c0316f77eefa4c
Kirros <miroslavzurek@gmail.com>
2015-06-26 00:26:41 +0200

    Initial commit

---
+++ src/fizzbuzz.py
@@ -0,0 +1,26 @@
+__author__ = 'Kirros'
+import sys
+
+
+def fizzbuzz(x, y, i):
+    if i % x == 0:
+        if i % y == 0:
+            return "FB"
+        return "F"
+    elif i % y == 0:
+        return "B"
+    else:
+        return str(i)
+
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    if test == "":
+        continue
+    test = (tuple(map(int, test.split(" "))))
+    print(" ".join(map(lambda x: fizzbuzz(test[0], test[1], x), range(1, test[2] + 1))))
+
+test_cases.close()
+
+
+
